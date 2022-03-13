50ac423cf70186f70cecf72b5a251b907950f3fe
Julian <julianung94@gmail.com>
2022-03-13 17:56:26 +0100

    check directory

--- src/modules/player.py
+++ src/modules/player.py
@@ -1,9 +1,10 @@
 import numpy as np
+from src.checks import player as check
 
 
 def constant_binary_distribution(types, rates, size_population):
 
-    constant_binary_distribution_checks(types, rates, size_population)
+    check.constant_binary_distribution_checks(types, rates, size_population)
 
     players = np.array([])
     for rate in enumerate(rates):
@@ -13,14 +14,3 @@ def constant_binary_distribution(types, rates, size_population):
     players = list(players)
 
     return players
-
-
-# TODO in its own script -> create sub-directory for each module
-def constant_binary_distribution_checks(types, rates, size_population):
-
-    # rate checks
-    rates_arr = np.array(rates)
-    if not 0 <= rates_arr.any() <= 1:
-        raise Exception('rates must be in range of [0, 1]')
-    elif rates_arr.sum() != 1:
-        raise Exception('population rates do not add up to 1')

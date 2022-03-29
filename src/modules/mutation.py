0150d044885da661a31df812c435ba1f1a2cec43
Julian <julianung94@gmail.com>
2022-03-29 11:44:43 +0200

    mutation algorithm improved

--- src/modules/mutation.py
+++ src/modules/mutation.py
@@ -12,16 +12,13 @@ def binary_uncorrelated(types, replications, size_population, mutation_rate):
     induce_mutation = np.random.choice(2, size_population, p=[1 - mutation_rate, mutation_rate])
 
     # mutations TODO numpy methods should exist that are more efficient than looping
-    mutated_players = np.array([])
-    for x in zip(players, induce_mutation):
-        if x[1] == 0:
-            mutated_players = np.append(mutated_players, x[0])
-        else:
-            mutated_players = np.append(mutated_players, abs(x[1]-1))
+    for i in range(len(induce_mutation)):
+        if induce_mutation[i] == 1:
+            players[i] = abs(players[i] - 1)
 
     # player to rates
-    rate_dove = sum(np.isin(mutated_players, 0)) / size_population
-    rate_hawk = sum(np.isin(mutated_players, 1)) / size_population
+    rate_dove = sum(np.isin(players, 0)) / size_population
+    rate_hawk = sum(np.isin(players, 1)) / size_population
     replications = [rate_dove, rate_hawk]
-    print(replications)
+
     return replications

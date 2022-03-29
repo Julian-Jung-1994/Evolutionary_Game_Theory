a451ae677d7a4ffb9fa4b15ccc5734a72f9efe95
Julian <julianung94@gmail.com>
2022-03-29 11:45:04 +0200

    new parameters for hawk-dove

--- src/modules/replication.py
+++ src/modules/replication.py
@@ -5,14 +5,14 @@ def hawk_dove(matches):
 
     for match in matches:
         if match == (0, 0):
-            replication_dove += 1.2
-            replication_dove += 1.2
+            replication_dove += 8
+            replication_dove += 8
         elif match == (0, 1) or match == (1, 0):
-            replication_dove += 0.6
-            replication_hawk += 1.4
+            replication_dove += 5
+            replication_hawk += 10
         elif match == (1, 1):
-            replication_hawk += 0.1
-            replication_hawk += 0.1
+            replication_hawk += 1
+            replication_hawk += 1
 
     rate_dove = replication_dove / (replication_dove + replication_hawk)
     rate_hawk = 1 - rate_dove
